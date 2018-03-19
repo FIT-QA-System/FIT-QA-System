72a408efd748728d97dc7e286bf7f9e67acb80ff
oliwang <Dg1AvHjj>
2018-03-19 12:02:58 -0400

    course info for some courses

--- website/FIT_QA_SYSTEM/QA/question_answering.py
+++ website/FIT_QA_SYSTEM/QA/question_answering.py
@@ -1,11 +1,15 @@
 from .models import *
 import re
+import spacy
 
 def answer(question):
     cat = categorize_questions(question)
 
     if cat == "Location":
         result = answer_location(question)
+        return result
+    elif cat == "Class":
+        result = answer_class(question)
         return result
     else:
         return {"answer": "this is the answer"}
@@ -21,6 +25,8 @@ def typeof(question):
         t=0
     elif cat=="Building Hours":
         t=0
+    elif cat=="Class":
+        t=0
     return t
 
 
@@ -32,12 +38,12 @@ def categorize_questions(question):
     preprocessed_question = preprocess(question)
 
     categories = ["Location", "Instructor", "Class Time", "Classroom", "Office Location", "Office Hours",
-                  "Building Hours", "Contact", "Others"]
+                  "Building Hours", "Contact", "Others", "Class"]
 
     if "location" in preprocessed_question or "where" in preprocessed_question or "address" in preprocessed_question:
         return "Location"
     else:
-        return "Instructor"
+        return "Class"
 
 
 def answer_location(question):
@@ -66,6 +72,42 @@ def answer_location(question):
         b = Building.objects.get(building_name=place)
 
     return {"answer": b.street}
+
+
+def answer_class(question):
+    first_word = question.split(" ")[0].lower()
+
+    nlp = spacy.load("en_core_web_sm")
+
+    course = nlp(question).ents[0].text
+    print(course)
+
+    answer_course = Course.objects.get(title=course)
+
+    result = {}
+
+    if answer_course:
+        if first_word == "who":
+            result['answer'] = answer_course.instructor
+        elif first_word == "where":
+            result['answer'] = answer_course.place
+        elif first_word == "when":
+            result['answer'] = answer_course.days + " " + answer_course.time
+        elif first_word == "what":
+            if question.split(" ")[1].lower() == "time":
+                result['answer'] = answer_course.time
+            elif question.split(" ")[1].lower() == "days":
+                result['answer'] = answer_course.days
+        else:
+            result['answer'] = str(answer_course)
+
+    else:
+        result['answer'] = "Class not found"
+
+    return result
+
+
+
 
 
 
